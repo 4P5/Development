8c882c6be465b8a037f3144483e865b44697d484
4P5 <theprismarinescientist@gmail.com>
2023-07-19 16:03:41 +1200

    More area setup
    - Global variables like BEDROCK, COW which are known blocks/entities to use in testing.
    - Better logging

--- test_handler.lua
+++ test_handler.lua
@@ -1,7 +1,7 @@
 -- Registers unit tests from the `tests` directory.
 local unit_tests = {}
 local n_tests = 0
-log("§b§l[DEV] §eLoading tests")
+logJson("\n\n\n§b§l[DEV] §eLoading tests\n")
 for _, path in pairs(listFiles("tests", true)) do
     local api, name = path:gsub("tests.",""):gsub(".lua",""):match("(.*)%.(.*)")
     unit_tests[api] = unit_tests[api] or {}
@@ -11,18 +11,19 @@ for _, path in pairs(listFiles("tests", true)) do
     }
     n_tests = n_tests + 1
 end
-log("§b§l[DEV] §eFound §b" .. n_tests .. " §etests")
 
+logJson("§b§l[DEV] §eFound §b" .. n_tests .. " §etests\n")
 for api, tests in pairs(unit_tests) do
-    log("§b§l[DEV] §eRunning tests for §b" .. api)
+    logJson("§b§l[DEV] §eRunning tests for §b" .. api .. "\n")
     for i, test in pairs(tests) do
         local result, expected = require(test.path)
         if result == expected then
-            log("§b§l[DEV] §2— §7" .. test.name .. " §2passed")
+            logJson("§b§l[DEV] §2— §7" .. test.name .. " §2passed\n")
         else
-            log("§b§l[DEV] §c— §7" .. test.name .. " §cfailed")
-            log("§b§l[DEV] §c—— §cExpected: §7" .. tostring(expected))
-            log("§b§l[DEV] §c—— §cGot: §7" .. tostring(result))
+            logJson("§b§l[DEV] §c— §7" .. test.name .. " §cfailed\n")
+            logJson("§b§l[DEV] §c—— §cExpected: §7" .. tostring(expected) .. "\n")
+            logJson("§b§l[DEV] §c—— §cGot: §7" .. tostring(result) .. "\n")
         end
     end
 end
+logJson("§b§l[DEV] §eFinished testing!\n\n\n")
--- test_setup.lua
+++ test_setup.lua
@@ -1,15 +1,36 @@
+-- player
 host:sendChatCommand("execute in minecraft:overworld run tp @s 100 101 100 0 0")
+host:sendChatCommand("effect clear")
+
+-- test area
+host:sendChatCommand("execute positioned 100 100 100 run fill ~-32 ~-24 ~-32 ~32 ~-16 ~32 minecraft:air")
+host:sendChatCommand("execute positioned 100 100 100 run fill ~-32 ~-16 ~-32 ~32 ~-8 ~32 minecraft:air")
+host:sendChatCommand("execute positioned 100 100 100 run fill ~-32 ~-8 ~-32 ~32 ~-0 ~32 minecraft:air")
+host:sendChatCommand("execute positioned 100 100 100 run fill ~-32 ~0 ~-32 ~32 ~8 ~32 minecraft:air")
+host:sendChatCommand("execute positioned 100 100 100 run fill ~-32 ~8 ~-32 ~32 ~16 ~32 minecraft:air")
+host:sendChatCommand("execute positioned 100 100 100 run fill ~-32 ~16 ~-32 ~32 ~24 ~32 minecraft:air")
+
 host:sendChatCommand("execute positioned 100 100 100 run fill ~-16 ~ ~-16 ~16 ~ ~16 minecraft:stone")
 host:sendChatCommand("execute positioned 100 100 100 run fillbiome ~-16 ~-8 ~-16 ~16 ~8 ~16 minecraft:old_growth_birch_forest")
-host:sendChatCommand("fill 100 101 100 100 319 100 air")
 
-host:sendChatCommand("time set 24000t")
-host:sendChatCommand("weather clear")
+host:sendChatCommand("fill 100 101 100 100 319 100 air") -- skylight access
 
--- blocks to test
+-- test blocks
 host:sendChatCommand("setblock 105 101 105 minecraft:light")
 host:sendChatCommand("setblock 105 101 104 minecraft:bedrock")
 host:sendChatCommand("setblock 105 101 103 minecraft:redstone_block")
 
--- entities to test
+-- world
+host:sendChatCommand("time set 24000t")
+host:sendChatCommand("weather clear")
+
+-- entities
 host:sendChatCommand("summon cow 100 102 104 {NoAI:1b,UUID:[I;1,2,3,4]}")
+
+-- globals
+LIGHT = world.getBlockState(105,101,105)
+BEDROCK = world.getBlockState(105,101,104)
+REDSTONE_BLOCK = world.getBlockState(105,101,103)
+COW = world.getEntity("00000001-0000-0002-0000-000300000004")
+
+
